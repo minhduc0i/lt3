d0063f6f221de2113f78cc88bf435821eae2b63c
minhduc0i <nguyenvanduc30052008@gmail.com>
2025-02-18 15:06:52 +0700

    Update 4.php

--- 4.php
+++ 4.php
@@ -15,7 +15,7 @@ function inDanhSachSinhVien($students) {
     usort($students, function($a, $b) {
         return strcmp($a["hoTen"], $b["hoTen"]);
     });
-    print_r($students);
+    return $students;
 }
 
 function danhSachSinhVienNu($students) {
@@ -34,7 +34,7 @@ function xapXepSinhVien($students) {
     usort($students, function($a, $b) {
         return $a["diemTB"] <=> $b["diemTB"];
     });
-    print_r($students);
+    return $students;
 }
 
 function timBanNuDiemTBCaoNhat($students) {
@@ -49,18 +49,36 @@ function timBanNuDiemTBCaoNhat($students) {
     return $nữSinhVien[0];
 }
 
-echo "Danh sách sinh viên sắp xếp theo tên:\n";
-inDanhSachSinhVien($students);
+function taoBangSinhVien($students) {
+    echo "<table border='1'>";
+    echo "<tr><th>Họ Tên</th><th>Ngày Sinh</th><th>Giới Tính</th><th>Điểm Toán</th><th>Điểm Văn</th><th>Điểm Tiếng Anh</th><th>Điểm TB</th></tr>";
+    foreach ($students as $student) {
+        echo "<tr>";
+        echo "<td>" . $student["hoTen"] . "</td>";
+        echo "<td>" . $student["ngaySinh"] . "</td>";
+        echo "<td>" . $student["gioiTinh"] . "</td>";
+        echo "<td>" . $student["toan"] . "</td>";
+        echo "<td>" . $student["van"] . "</td>";
+        echo "<td>" . $student["tiengAnh"] . "</td>";
+        echo "<td>" . number_format($student["diemTB"], 2) . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+}
 
-echo "\nDanh sách sinh viên nữ:\n";
-print_r(danhSachSinhVienNu($students));
+echo "<h3>Danh sách sinh viên sắp xếp theo tên:</h3>";
+taoBangSinhVien(inDanhSachSinhVien($students));
 
-echo "\nDanh sách sinh viên có điểm TB >= 8.0:\n";
-print_r(danhSachSinhVienDiemTB($students));
+echo "<h3>Danh sách sinh viên nữ:</h3>";
+taoBangSinhVien(danhSachSinhVienNu($students));
 
-echo "\nDanh sách sinh viên sắp xếp theo điểm TB:\n";
-xapXepSinhVien($students);
+echo "<h3>Danh sách sinh viên có điểm TB >= 8.0:</h3>";
+taoBangSinhVien(danhSachSinhVienDiemTB($students));
 
-echo "\nBạn nữ có điểm TB cao nhất:\n";
-print_r(timBanNuDiemTBCaoNhat($students));
+echo "<h3>Danh sách sinh viên sắp xếp theo điểm TB:</h3>";
+taoBangSinhVien(xapXepSinhVien($students));
+
+echo "<h3>Bạn nữ có điểm TB cao nhất:</h3>";
+$topFemale = timBanNuDiemTBCaoNhat($students);
+taoBangSinhVien([$topFemale]);
 ?>
